45363e2c0315555121b7aa80c5dc8b4339714af5
apurba-patra-gh <apurba.patra@growhut.in>
2025-08-02 23:22:03 +0530

    added

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,8 @@ const corsOptions = {
   origin: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'http://task-management-frontend.vercel.app'
+    'http://task-management-frontend.vercel.app',
+    /\.vercel\.app$/,
   ],
   credentials: true,
   optionsSuccessStatus: 200,
